a427bf80d8c04e31c76638f895d47e407b25b64b
lepelog <lepelog4@gmail.com>
2020-07-17 16:28:43 +0200

    let lz temp flags go from 0x to 8x

--- util.py
+++ util.py
@@ -39,19 +39,11 @@ def flag_id_to_sheet_rep(idx):
     elif idx >= 128 and idx < 192:
         return 'temp flag: '+idx_to_scene_flag(idx-128)
     elif idx >= 192 and idx < 255:
-        return 'loadzone temp flag: '+idx_to_scene_flag(idx-128)
+        return 'loadzone temp flag: '+idx_to_scene_flag(idx-192)
     elif idx < 0:
         return flag_id_to_sheet_rep(idx+255)
     else:
         raise Exception('invalid areaflagid!')
-
-# for 2 bytes as numbers extracts number in between with offset
-def extract_byte_between_2_bytes(b1, b2, bitoffset=4):
-    flagid = int('{:016b}'.format(int((struct.pack('b',b1)+struct.pack('b',b2)).hex(), 16))[bitoffset:bitoffset+8],2)
-    # convert byte to signed
-    if flagid >= 128:
-        flagid=flagid-256
-    return flagid
 
 def read_halfword(btes):
     return struct.unpack('>H',btes)[0]
